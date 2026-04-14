045fb7f485ecc790be097bed5008eb38ed7d0e9f
ashumaperamil <ashumaperamil@gmail.com>
2026-04-14 08:41:36 +0300

    Update register.js

--- api/register.js
+++ api/register.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
 
       // 3. Success redirect
      
-return res.redirect(303, 'https://pikofutsal.github.io/thanks.html')
+return res.redirect(303, 'https://pikofutsal.github.io/piko-futsal/thanks.html')
       
     } catch (err) {
       return res.status(500).json({ error: err.message })
